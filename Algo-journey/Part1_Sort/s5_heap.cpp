a54c7e2a0942bfd688acaacb2d817c9897d132b9
chanhocode <chjo1245@gmail.com>
2025-04-06 22:06:16 +0900

    TIL 2025.04.06: 알고리즘 복습 - Heap

---
+++ Algo-journey/Part1_Sort/s5_heap.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void heapify(vector<int>& arr, int n, int i) {
+    int largest = i;
+    int l = 2 * i + 1;
+    int r = 2 * i + 2;
+
+    if (l < n && arr[l] > arr[largest])
+        largest = l;
+    if (r < n && arr[r] > arr[largest])
+        largest = r;
+    if (largest != i) {
+        int temp = arr[i];
+        arr[i] = arr[largest];
+        arr[largest] = temp;
+        heapify(arr, n, largest);
+    }
+}
+
+void heapSort(vector<int>& arr) {
+    int n = arr.size();
+    
+    for (int i = n / 2 - 1; i >= 0; i--)
+        heapify(arr, n, i);
+
+    for (int i = n - 1; i >= 0; i--) {
+        int temp = arr[0];
+        arr[0] = arr[i];
+        arr[i] = temp;
+        heapify(arr, i, 0);
+    }
+}
+
+//int main() {
+//    vector<int> arr = { 4, 10, 3, 5, 1 };
+//    heapSort(arr);
+//
+//    for (int val : arr)
+//        cout << val << " ";
+//}
